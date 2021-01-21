6fb8487a4f9398d89f5831acfaf1577b226d8205
Kristen Jestin <kris.jestin@laposte.net>
2021-01-21 19:56:34 +0100

    feat: add toast system

--- src/views/Home/home.view.tsx
+++ src/views/Home/home.view.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useAlert } from 'react-alert'
 
 const Home = (): React.ReactElement => {
+	const alert = useAlert()
+	alert.success('Test toast message')
 	return <div>HOME</div>
 }
 
