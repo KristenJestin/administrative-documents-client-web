f341fa18346e34c4f6f84c7f90bdb65fe2aae863
Kristen Jestin <kris.jestin@laposte.net>
2021-01-23 17:23:00 +0100

    fix: remove alert test methods

--- src/views/Home/home.view.tsx
+++ src/views/Home/home.view.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { useAlert } from 'react-alert'
 
 const Home = (): React.ReactElement => {
-	const alert = useAlert()
-	alert.success('Test toast message')
 	return <div>HOME</div>
 }
 
