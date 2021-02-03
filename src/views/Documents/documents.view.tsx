8351635f607051ba3ab3d101e1550d4d471b5a86
Kristen Jestin <kris.jestin@laposte.net>
2021-02-03 09:14:00 +0100

    refactor: remove unused imports in documents page

--- src/views/Documents/documents.view.tsx
+++ src/views/Documents/documents.view.tsx
@@ -1,44 +1,14 @@
 // imports
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 
 import MainContainer from '../../components/main-container.component'
-import Document from '../../models/document/document.model'
-import AppRoute from '../../navigations/app-routes'
-import service from '../../services/document.service'
-
-// config
-type DocumentsState = {
-	documents: Document[]
-	loading: boolean
-}
 
 // main
-const Documents = (): React.ReactElement => {
-	// hooks
-	const [pageState, setPageState] = useState<DocumentsState>({
-		documents: [],
-		loading: true,
-	})
-
-	// actions
-	useEffect(() => {
-		;(async () => {
-			const docs = (await service.latest()).data.result
-			setPageState(() => ({
-				documents: docs,
-				loading: false,
-			}))
-		})()
-	}, [])
-
-	// render
-	return (
-		<MainContainer title="Tous les Documents">
-			<h2>DOCUMENTS</h2>
-		</MainContainer>
-	)
-}
+const Documents = (): React.ReactElement => (
+	<MainContainer title="Tous les Documents">
+		<h2>DOCUMENTS</h2>
+	</MainContainer>
+)
 
 // exports
 export default Documents
